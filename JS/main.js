b0b7f5a430d9254537ff182edc85537c5748875c
lucasaraujope <93890096+lucasaraujope@users.noreply.github.com>
2023-11-08 17:45:59 -0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -24,5 +24,67 @@ function trocar(){
     let aux = videoAtual;
     videoAtual = videoNovo;
     videoNovo = aux;
-    
 }
+    
+function changeVideo(embedUrl) {
+        document.getElementById("video").src = embedUrl;
+}
+
+function changeVideo2(embedUrl) {
+        document.getElementById("video2").src = embedUrl;
+}
+
+  
+const checkbox = document.getElementById('marcar');
+checkbox.addEventListener('change', (event) => {
+const sessao = document.getElementById('sessao')
+        if(event.target.checked) {
+            sessao.className = "ocultar"
+        } else {
+            sessao.className = "mostrar"
+        }
+})
+    
+        const checkbox2 = document.getElementById('marcar2');
+        checkbox2.addEventListener('change', (event) => {
+            const sessao = document.getElementById('sessao2')
+            if(event.target.checked) {
+                sessao2.className = "ocultar"
+            } else {
+                sessao2.className = "mostrar"
+            }
+        })
+     
+
+        const checkbox3 = document.getElementById('marcar3');
+        checkbox3.addEventListener('change', (event) => {
+            const sessao = document.getElementById('sessao3')
+            if(event.target.checked) {
+                sessao3.className = "ocultar"
+            } else {
+                sessao3.className = "mostrar"
+            }
+        })
+      
+
+        const checkbox4 = document.getElementById('marcar4');
+        checkbox4.addEventListener('change', (event) => {
+            const sessao = document.getElementById('sessao4')
+            if(event.target.checked) {
+                sessao4.className = "ocultar"
+            } else {
+                sessao4.className = "mostrar"
+            }
+        })
+     
+
+        const checkbox5 = document.getElementById('marcar5');
+        checkbox5.addEventListener('change', (event) => {
+            const sessao = document.getElementById('sessao5')
+            if(event.target.checked) {
+                sessao5.className = "ocultar"
+            } else {
+                sessao5.className = "mostrar"
+            }
+        })
+      
